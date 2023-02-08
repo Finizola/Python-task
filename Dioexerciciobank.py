6990c448f769187ec9e6d59657523d561a4d3120
Finizola <leo.finizola@gmail.com>
2023-02-08 17:20:59 -0300

    Create Dioexerciciobank.py

---
+++ Dioexerciciobank.py
@@ -0,0 +1,26 @@
+import math
+
+print("SISTEMA BANCARIO V0.0001")
+dinheiro = 0
+while True:
+    try:
+        menu = int(input("digite a opcao desejada: \n 1: DEPOSITO \n 2: SAQUE \n 3: EXTRATO \n 4: Sair\n\n"))
+    
+    except:
+        print("\n \n DIGITE UM NUMERO INTEIRO: ")
+
+    if(menu ==  1):
+        print("Deposito \n\n")
+        dinheiro += int(input("digite o valor para deposito: \n"))
+    
+    if(menu ==  2):
+        print("Saque\n\n")
+        dinheiro -= int(input("digite o valor para saque: \n"))
+    
+    if(menu ==  3):
+        print("Extrato\n\n")
+        print('o dinheiro na conta e: {}'.format(dinheiro))
+
+    if(menu ==  4):
+        print("\n SAINDO,OBRIGADO POR UTILIZAR")
+        break

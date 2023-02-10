5d5741ea09dd70fc76a9963122167a362b9af37a
Finizola <leo.finizola@gmail.com>
2023-02-10 13:05:22 -0300

    Update Dioexerciciobank.py

--- Dioexerciciobank.py
+++ Dioexerciciobank.py
@@ -2,6 +2,7 @@ import math
 
 print("SISTEMA BANCARIO V0.0001")
 dinheiro = 0
+extrato = []
 while True:
     try:
         menu = int(input("digite a opcao desejada: \n 1: DEPOSITO \n 2: SAQUE \n 3: EXTRATO \n 4: Sair\n\n"))
@@ -12,6 +13,8 @@ while True:
     if(menu ==  1):
         print("Deposito \n\n")
         dinheiro += int(input("digite o valor para deposito: \n"))
+        extrato.append(dinheiro)
+
     
     if(menu ==  2):
         print("Saque\n\n")
@@ -20,6 +23,7 @@ while True:
     if(menu ==  3):
         print("Extrato\n\n")
         print('o dinheiro na conta e: {}'.format(dinheiro))
+        print("\n \n os valores de deposito foram: {}\n\n".format(extrato[:]))
 
     if(menu ==  4):
         print("\n SAINDO,OBRIGADO POR UTILIZAR")

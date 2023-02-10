abae505d484a5990aa34b9d96b957f3cd3d20ac1
Finizola <leo.finizola@gmail.com>
2023-02-10 14:10:57 -0300

    Update Dioexerciciobank.py

--- Dioexerciciobank.py
+++ Dioexerciciobank.py
@@ -2,6 +2,7 @@ import math
 
 print("SISTEMA BANCARIO V0.0001")
 dinheiro = 0
+saques = 0
 extrato = []
 while True:
     try:
@@ -12,18 +13,36 @@ while True:
 
     if(menu ==  1):
         print("Deposito \n\n")
-        dinheiro += int(input("digite o valor para deposito: \n"))
+        dinheiro += float(input("digite o valor para deposito: \n"))
         extrato.append(dinheiro)
 
     
     if(menu ==  2):
         print("Saque\n\n")
-        dinheiro -= int(input("digite o valor para saque: \n"))
+        if (dinheiro == 0):
+            print("\n sem dinheiro na conta!\n")
+        
+        elif (saques == 3):
+            print("limite de saques alcançado")
+
+        else:
+            retiradas = float(input("digite o valor para saque: \n"))
+            if ((dinheiro - retiradas )< 0):
+                print("ERRO,verifique o extrato bancario")
+            else:
+                saques += 1
+                dinheiro -= retiradas
+                print("\nsaques feitos: {}\n".format(saques))
     
     if(menu ==  3):
-        print("Extrato\n\n")
-        print('o dinheiro na conta e: {}'.format(dinheiro))
-        print("\n \n os valores de deposito foram: {}\n\n".format(extrato[:]))
+        if(extrato == []):
+            print("nao foram realizadas movimentacoes")
+            
+        else:
+            print("Extrato\n\n")
+            print(f'o dinheiro na conta e: R${dinheiro:.2f}')
+            print("\n \n os valores de deposito foram: R${}\n\n".format(extrato[:]))
+
 
     if(menu ==  4):
         print("\n SAINDO,OBRIGADO POR UTILIZAR")
